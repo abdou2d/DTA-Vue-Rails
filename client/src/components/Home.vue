d7f976f983198864613041e63e8bed5b21596a5a
Nicholas Shirley <nicholas@reallymy.email>
2017-11-02 14:10:58 +0100

    cleanup and handle errors

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,14 +1,23 @@
 <template>
   <div>
+
+    <!-- Show if validate token returns user info on page load or user is logged in -->
     <div v-if="Store.user.isAuth">
+      <!-- Define columns here and apply "column" class to both components -->
       <div class="columns">
+
+        <!-- Content components -->
         <Notes></Notes>
         <UpdateUser></UpdateUser>
+
       </div>
     </div>
+
+    <!-- Show if validate token returns 401 or user is not logged in -->
     <div v-else>
         <Login></Login>
     </div>
+    
   </div>
 </template>
 
--- client/src/components/UpdateUser.vue
+++ client/src/components/UpdateUser.vue
@@ -1,36 +1,69 @@
 <template>
-	<div class="column">
+  <!-- column for user content, contained inside "columns" class on main page -->
+  <div class="column">
     <h1 class="title">Update user info</h1>
-    <div class="field">
-      <label class="label">Current password</label>
-      <input class="input" v-model="Store.user.email">
+
+    <!-- If errors, log to user -->
+    <div class="box" v-show="errors.userUpdate.length > 0">
+      <ul v-for="error in errors.userUpdate">
+        <li class="help has-text-danger"> {{ error }}</li>
+      </ul>
+    </div>
+
+    <!-- If success, log to user -->
+    <div class="box" v-show="success.userSuccessMessage.length > 0">
+      <p class="has-text-success"> {{ success.userSuccessMessage }}</p>
+    </div>
+
+    <!-- Update user input fields -->
+    <div class="field has-text-left">
+      <label class="label">New Email</label>
+      <input class="input" :placeholder="Store.user.email" v-model="userInfo.email">
+    </div>
+    <div class="field has-text-left">
+      <label class="label">New username</label>
+      <input class="input" :placeholder="Store.user.user_name" v-model="userInfo.user_name">
     </div>
     <div class="field">
-      <label class="label">Current username</label>
-      <input class="input" v-model="Store.user.user_name">
-    </div>
-    <div>
-      <button class="button is-info" @click="submit">Submit</button>
+      <div class="control">
+        <button class="button is-info" @click="submit">Submit</button>
+      </div>
     </div>
 
     <div><hr /></div>
 
-    <div class="field">
-      <label class="label" v-show="message"></label>
+    <!-- If errors, log to user -->
+    <div class="box" v-show="errors.password.length > 0">
+      <ul v-for="error in errors.password">
+        <li class="help has-text-danger"> {{ error }}</li>
+      </ul>
+    </div>
+
+    <!-- If success, log to user -->
+    <div class="box" v-show="success.passwordSuccessMessage.length > 0">
+      <p class="has-text-success"> {{ success.passwordSuccessMessage }}</p>
+    </div>
+
+    <!-- Update user password fields -->
+    <div class="field has-text-left">
       <label class="label">Current password</label>
       <input class="input" v-model="password.current_password" type="password">
     </div>
-    <div class="field">
+    <div class="field has-text-left">
       <label class="label">New password</label>
       <input class="input" v-model="password.password" type="password">
     </div>
-    <div class="field">
+    <div class="field has-text-left">
       <label class="label">Confirm new password</label>
       <input type="password" class="input" v-model="password.password_confirmation">
     </div>
-    <div>
-      <button class="button is-info" @click="changePassword">Submit</button>
+    <div class="field">
+      <div class="control">
+        <button class="button is-info" @click="changePassword">Submit</button>
+      </div>
     </div>
+
+  <!-- end of content container -->
   </div>
 </template>
 
@@ -38,6 +71,9 @@
 
   import axios from 'axios';
   import { Store } from '@/store/Store';
+
+  const USER_UPDATE_URL = 'http://localhost:3000/api/auth';
+  const PASSWORD_UPDATE_URL = 'http://localhost:3000/api/auth/password';
 
   export default {
 
@@ -49,46 +85,85 @@
           password_confirmation: '',
           current_password: '',
         },
-        message: null,
+        userInfo: {
+          user_name: '',
+          email: '',
+        },
+        errors: {
+          userUpdate: [],
+          password: [],
+        },
+        success: {
+          userSuccessMessage: '',
+          passwordSuccessMessage: '',  
+        },
       }
     },
 
     methods: {
 
       submit() {
+        // Clear old messages, if any
+        Object.keys(this.errors).forEach(value => this.errors[value] = []);
+        Object.keys(this.success).forEach(value => this.success[value] = '');
 
-        const USER_UPDATE_URL = 'http://localhost:3000/api/auth';
+        // Pick up info from form
+        const userUpdate = {
+          email: this.userInfo.email,
+          user_name: this.userInfo.user_name,
+        };
 
-        axios.patch(USER_UPDATE_URL, {
-          email: this.Store.user.email,
-          user_name: this.Store.user.user_name,
-        })
+        // Strip out any blanks from form so they do not overwrite current values
+        Object.keys(userUpdate)
+          .forEach(k => (!userUpdate[k] && userUpdate[k] !== undefined) && delete userUpdate[k]);
+
+        // Send user update info
+        axios.patch(USER_UPDATE_URL, userUpdate)
           .then((response) => {
+            // Update user store
             this.Store.user.email = response.data.data.email;
             this.Store.user.user_name = response.data.data.user_name;
-            console.log(response.message);
-            //this.messsage = response.message;
+
+            // Clear form data
+            this.userInfo.user_name = '';
+            this.userInfo.email = '';
+
+            // Show success
+            this.success.userSuccessMessage = 'Your user info has been updated';
           })
+          .catch((error) => {
+            error.response.data.errors.full_messages.forEach((errorText) => {
+              this.errors.userUpdate.push(errorText);
+            });
+        });
       },
 
       changePassword() {
+        // Clear old messages, if any
+        Object.keys(this.errors).forEach(value => this.errors[value] = []);
+        Object.keys(this.success).forEach(value => this.success[value] = '');
 
-        const PASSWORD_UPDATE_URL = 'http://localhost:3000/api/auth/password';
-
+        // No need to strip blanks, will be picked up by catch
         axios.patch(PASSWORD_UPDATE_URL, {
           password: this.password.password,
           password_confirmation: this.password.password_confirmation,
           current_password: this.password.current_password,
         })
-          .then((response) => {
-            this.password.password = '';
-            this.password.password_confirmation = '';
-            this.password.current_password = '';
-          })
-          .catch((response) => {
-            console.log(response.errors);
-          })
-      }
+        .then((response) => {
+          // Reset form values
+          this.password.password = '';
+          this.password.password_confirmation = '';
+          this.password.current_password = '';
+
+          // Display success message to user
+          this.success.passwordSuccessMessage = 'Your password has been successfully changed'
+        })
+        .catch((error) => {
+          error.response.data.errors.full_messages.forEach((errorText) => {
+            this.errors.password.push(errorText);
+          });
+        })
+      },
     },
   };
 	
